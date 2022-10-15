429c4020baacdb2d38160b0d1d0eaf6b2b29ff6b
gh0st2002 <marciopvpsubs@gmail.com>
2022-10-15 00:02:04 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App(){
+ return (
+<>
+    <h1 className='font-bold text-sm text-violet-500'> Hello Word !</h1>
+
+    <button className='  bg-cyan-300 font-medium px-10 py-3 rounded text-white hover:bg-cyan-500' >
+      Amo você
+    </button>
+    </>
+  )
+}
